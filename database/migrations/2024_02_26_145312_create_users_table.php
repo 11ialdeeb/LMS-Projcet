d0ae2771a7945e7327bc6558ff8f5548a2d3649b
ialdeeb11 <11.ialdeeb@gmail.com>
2024-03-02 16:34:42 +0300

    creating tables

--- database/migrations/2024_02_26_145312_create_users_table.php
+++ database/migrations/2024_02_26_145312_create_users_table.php
@@ -14,10 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email');
             $table->string('password');
-            $table->rememberToken();
+
+            $table->boolean('borrower');
+
+
+            $table->boolean('isPublisher');
             $table->timestamps();
         });
     }
--- database/migrations/2024_03_02_130855_add_new_column_to_users_table.php
+++ database/migrations/2024_03_02_130855_add_new_column_to_users_table.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::table('users', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('borrowed_book_id')->nullalbe();
+
+            $table->foreign('borrowed_book_id')->references('id')->on('books')->onDelete('cascade');
         });
     }
 
@@ -23,6 +24,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 };
